6c072b28bcf866aa7947fcfd2dbbb16973b1d4a1
drusjs <magnojoky962@gmail.com>
2024-02-02 15:17:28 +0300

    add swiper

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-cases', {
+    loop: true,
+    spaceBetween: 200,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+  
