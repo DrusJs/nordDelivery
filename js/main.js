1baaf4b0ed555c89c062f1deda19dbc2466e0946
drusjs <magnojoky962@gmail.com>
2024-02-02 15:47:34 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-cases', {
+const swiperCases = new Swiper('.swiper-cases', {
     loop: true,
     spaceBetween: 200,
 
@@ -7,5 +7,23 @@ const swiper = new Swiper('.swiper-cases', {
       prevEl: '.swiper-button-prev',
     },
 
-  });
+});
+const swiperReviews = new Swiper('.swiper-reviews', {
+    loop: true,
+    spaceBetween: 200,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+});
+
+if (document.querySelector('.accordion-head')) {
+    document.querySelectorAll('.accordion-head').forEach(el => {
+        el.addEventListener('click', function() {
+            this.parentElement.classList.toggle('active')
+        })
+    })
+}
   
