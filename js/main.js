3b45971bdb39087c1fd785cad3f74e44d3fb5028
Беликов Андрей Юрьевич <magnojoky962@gmail.com>
2024-02-03 15:02:23 +0300

    update

--- js/main.js
+++ js/main.js
@@ -27,4 +27,29 @@ if (document.querySelector('.accordion-head')) {
         })
     })
 }
+if (document.querySelector('.js-open-modal')) {
+    document.querySelectorAll('.js-open-modal').forEach(el => {
+        el.addEventListener('click', function() {
+            let active = document.querySelector('.modal-wrapper.active')
+            if (active) {
+                active.classList.remove('active')
+            }
+            document.getElementById(this.dataset.modal).classList.add('active')
+        })
+    })
+}
+if (document.querySelector('.js-close-modal')) {
+    document.querySelectorAll('.js-close-modal').forEach(el => {
+        el.addEventListener('click', function() {
+            this.closest('.modal-wrapper').classList.remove('active')
+        })
+    })
+    document.querySelectorAll('.modal-wrapper').forEach(el => {
+        el.addEventListener('click', function(event) {
+            if (event.target.classList.contains('modal-wrapper')) {
+                this.classList.remove('active')
+            }         
+        })
+    })
+}
   
