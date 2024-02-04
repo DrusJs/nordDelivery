646bc344f81668e816a4f3ce389e5f6f8b8311af
Беликов Андрей Юрьевич <magnojoky962@gmail.com>
2024-02-04 12:54:52 +0300

    update modals

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ if (document.querySelector('.js-open-modal')) {
                 active.classList.remove('active')
             }
             document.getElementById(this.dataset.modal).classList.add('active')
+            document.body.classList.add('no-scroll')
         })
     })
 }
@@ -42,14 +43,29 @@ if (document.querySelector('.js-close-modal')) {
     document.querySelectorAll('.js-close-modal').forEach(el => {
         el.addEventListener('click', function() {
             this.closest('.modal-wrapper').classList.remove('active')
+            document.body.classList.remove('no-scroll')
         })
     })
     document.querySelectorAll('.modal-wrapper').forEach(el => {
         el.addEventListener('click', function(event) {
             if (event.target.classList.contains('modal-wrapper')) {
                 this.classList.remove('active')
+                document.body.classList.remove('no-scroll')
             }         
         })
     })
 }
-  
+
+let phoneInput = document.querySelectorAll(".js-phone");
+
+phoneInput.forEach(input=> {
+    ////////////////////////////////////////////////
+    // phoneMask - объект маски для инпута телефона
+    // Поле phoneMask.unmaskedValue — переводит номер из маски в обычное число
+    // Например при значении "+7(900)111-11-11" в поле unmaskedValue будет "79001111111"
+    // Поле phoneMask.masked.isComplete — проверяет полностью ли введен номер
+    ////////////////////////////////////////////////
+    let phoneMask = new IMask(input, {
+        mask: "+{7}(000)000-00-00",
+    });
+})
