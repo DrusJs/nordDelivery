e4187119b44cb7e69fbfe0d7e61d6903acfe6590
Беликов Андрей Юрьевич <magnojoky962@gmail.com>
2024-02-05 21:32:02 +0300

    update

--- js/main.js
+++ js/main.js
@@ -103,3 +103,30 @@ checkBlocksVisibility();
 window.addEventListener('scroll', function() {
     checkBlocksVisibility();
 });
+SmoothScroll({
+    // Время скролла 400 = 0.4 секунды
+    animationTime : 800,
+    // Размер шага в пикселях
+    stepSize : 75,
+    // Дополнительные настройки:
+
+    // Ускорение
+    accelerationDelta : 30,
+    // Максимальное ускорение
+    accelerationMax : 10,
+
+    // Поддержка клавиатуры
+    keyboardSupport : true,
+    // Шаг скролла стрелками на клавиатуре в пикселях
+    arrowScroll : 50,
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm : true,
+    pulseScale : 4,
+    pulseNormalize : 1,
+
+    // Поддержка тачпада
+    touchpadSupport : true,
+})
+console.log(1)
